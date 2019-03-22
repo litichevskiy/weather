e804ce9812c69bed93faa7d86b3bec4c0ac4cb24
litichevskiy <kunin25@ukr.net>
2019-03-22 18:51:22 +0200

    test

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ const NotFound = require('./components/NotFound');
 const Menu = require('./components/Menu');
 const SavedCities = require('./components/SavedCities');
 
+
 new BlockSearch({ container: document.querySelector('.blockSearch') });
 new Header({ container: document.querySelector('.header') });
 new ListSities({ container: document.querySelector('.listSities') });
